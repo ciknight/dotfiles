6ed161ec8b444b19b4e7b4e593112f88b7b2f37f
Justin Moen <jamoen7@gmail.com>
2015-06-06 13:40:01 -0700

    massive, unorganized changes

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,7 @@
 export EDITOR=/usr/local/bin/vim
 export VISUAL=/usr/local/bin/vim
 
-export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/usr/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin"
+export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/usr/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin:/Users/Justin/Library/Android/sdk/platform-tools"
 
 export TERM=xterm-256color        # for common 256 color terminals (e.g. gnome-terminal)
 export TERM=screen-256color       # for a tmux -2 session (also for screen)
