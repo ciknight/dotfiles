7a4f985caba2fc6595a229e86b2250fb1d1f9c69
Justin Moen <jamoen7@gmail.com>
2015-06-21 17:43:44 -0700

    remove minibufexpl

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,11 @@
 export EDITOR=/usr/local/bin/vim
 export VISUAL=/usr/local/bin/vim
 
-export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/usr/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin:/Users/Justin/Library/Android/sdk/platform-tools"
+export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/usr/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin:/Users/Justin/Library/Android/sdk/platform-tools:/Users/Justin/bin/apache-ant-1.9.5/bin"
+
+export ANT_HOME=/Users/Justin/bin/apache-ant-1.9.5
+export NVIM_LISTEN_ADDRESS=/tmp/neovim/neovim
+
 
 export TERM=xterm-256color        # for common 256 color terminals (e.g. gnome-terminal)
 export TERM=screen-256color       # for a tmux -2 session (also for screen)
