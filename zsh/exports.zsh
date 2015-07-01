acb2800ede94845f8a339d8d4a5e8b604ab348a2
Justin Moen <jamoen7@gmail.com>
2015-07-01 15:47:32 -0700

    Default editor is neovim. Remove boot2docker stuff.

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,7 @@
-export EDITOR=/usr/local/bin/vim
-export VISUAL=/usr/local/bin/vim
+export EDITOR=/usr/local/bin/nvim
+export VISUAL=/usr/local/bin/nvim
 
-export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/usr/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin:/Users/Justin/Library/Android/sdk/platform-tools:/Users/Justin/bin/apache-ant-1.9.5/bin"
+export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/usr/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin:/Users/Justin/Library/Android/sdk/platform-tools"
 
 export ANT_HOME=/Users/Justin/bin/apache-ant-1.9.5
 export NVIM_LISTEN_ADDRESS=/tmp/neovim/neovim
@@ -9,8 +9,3 @@ export NVIM_LISTEN_ADDRESS=/tmp/neovim/neovim
 
 export TERM=xterm-256color        # for common 256 color terminals (e.g. gnome-terminal)
 export TERM=screen-256color       # for a tmux -2 session (also for screen)
-
-# boot2docker
-export DOCKER_TLS_VERIFY=1
-export DOCKER_HOST=tcp://192.168.59.103:2376
-export DOCKER_CERT_PATH=/Users/justin/.boot2docker/certs/boot2docker-vm
