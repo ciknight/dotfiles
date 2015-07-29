5f2e47b1f7034e96941511ec8f98347ab6197ed0
Justin Moen <jamoen7@gmail.com>
2015-07-29 13:23:24 -0700

    Export $KARMIC_HOME

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -1,7 +1,7 @@
 export EDITOR=/usr/local/bin/nvim
 export VISUAL=/usr/local/bin/nvim
 
-export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/usr/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin:/Users/Justin/Library/Android/sdk/platform-tools:/Applications/Racketv6.2/bin"
+export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/usr/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin:/Users/Justin/Library/Android/sdk/platform-tools:/Applications/Racketv6.2/bin:/Users/Justin/bin/didit-cli-client:/Users/justin/bin/neo4j-community-2.2.3/bin"
 
 export ANT_HOME=/Users/Justin/bin/apache-ant-1.9.5
 export NVIM_LISTEN_ADDRESS=/tmp/neovim/neovim
@@ -9,3 +9,5 @@ export NVIM_LISTEN_ADDRESS=/tmp/neovim/neovim
 
 export TERM=xterm-256color        # for common 256 color terminals (e.g. gnome-terminal)
 export TERM=screen-256color       # for a tmux -2 session (also for screen)
+
+export KARMIC_HOME=/Users/Justin/Karmic
