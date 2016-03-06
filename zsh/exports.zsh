0d40b8c9c9993e299d1898972c9b79109bb94c9e
Justin Moen <justin@justins-mbp.attlocal.net>
2016-03-05 18:10:00 -0800

    Visual cleanup

--- zsh/exports.zsh
+++ zsh/exports.zsh
@@ -2,7 +2,7 @@ export EDITOR=/usr/local/bin/nvim
 export VISUAL=/usr/local/bin/nvim
 export MYVIMRC=/Users/Justin/.config/nvim/init.vim
 
-export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/Users/Justin/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin:/Users/Justin/Library/Android/sdk/platform-tools:/Applications/Racketv6.2/bin:/Users/Justin/bin/didit-cli-client:/Users/justin/bin/neo4j-community-2.2.3/bin"
+export PATH="/Library/Frameworks/Python.framework/Versions/3.4/bin:/Library/Frameworks/Python.framework/Versions/2.7/bin:/usr/local/heroku/bin:/Applications/Postgres.app/Contents/Versions/9.4/bin:/Users/Justin/bin/arcanist/bin:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/deployd/bin:/usr/local/MacGPG2/bin:/usr/local/sbin:/Users/Justin/bin:/Users/Justin/Library/Android/sdk/platform-tools:/Applications/Racketv6.2/bin:/Users/Justin/bin/didit-cli-client:/Users/justin/bin/neo4j-community-2.2.3/bin:/Users/justin/.rvm/"
 
 export ANT_HOME=/Users/Justin/bin/apache-ant-1.9.5
 export NVIM_LISTEN_ADDRESS=/tmp/neovim/neovim
@@ -12,3 +12,4 @@ export TERM=xterm-256color        # for common 256 color terminals (e.g. gnome-t
 export TERM=screen-256color       # for a tmux -2 session (also for screen)
 
 export KARMIC_HOME=/Users/Justin/Karmic
+export WORKON_HOME=~/.config/virtualenvs
