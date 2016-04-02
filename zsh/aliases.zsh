7c127934af4251b90298c2dc8811526c5bb0ceae
Justin Moen <jamoen7@gmail.com>
2016-04-02 16:26:19 -0700

    random shit

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,9 +2,6 @@ alias gx="git diff | gitx"
 alias gsx="git diff --staged | gitx"
 alias gox="git diff origin/master | gitx"
 
-alias python='python3'
-alias pip='pip3'
-
 alias backend='cd ~/karmic/karmicapp/src && workon karmic'
 alias react='cd ~/karmic/karmic-style/frontend'
 
--- zsh/sources.zsh
+++ zsh/sources.zsh
@@ -1,7 +1,9 @@
-source /Library/Frameworks/Python.framework/Versions/3.4/bin/virtualenvwrapper.sh
+source /Library/Frameworks/Python.framework/Versions/3.5/bin/virtualenvwrapper.sh
+source /usr/local/bin/virtualenvwrapper.sh
 
 # for use with dark solarized colors
 # eval `gdircolors ~/dotfiles/zsh/dircolors-solarized/dircolors.256dark`
 
 source ~/dotfiles/zsh/tmuxinator.zsh
+
 
