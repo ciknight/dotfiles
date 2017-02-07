a64bd9e0d31bfd3d200530a7d3d8ff82f2f21308
Justin Moen <jamoen7@gmail.com>
2017-02-07 23:35:18 +0800

    setup

---
+++ remote/link.sh
@@ -0,0 +1,4 @@
+ln -s tmux/tmux.conf ~/.tmux.conf
+ln -s zsh/zshrc ~/.zshrc
+mkdir ~/.config
+ln -s nvim ~/.config
--- remote/provision.sh
+++ remote/provision.sh
@@ -4,9 +4,9 @@ sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:neovim-ppa/unstable
 sudo add-apt-repository ppa:tmate.io/archive
 sudo apt-get update
-sudo apt-get install -y git tmux mosh neovim python-dev python-pip python3-dev python3-pip docker-engine zsh
+sudo apt-get install -y git tmux mosh neovim python-dev python-pip python3-dev python3-pip docker.io zsh
 
 # Install oh-my-zsh
-curl -L http://install.ohmyz.sh | sh
+curl -L http://install.ohmyz.sh | zsh
 
 chsh -s $(which zsh)
